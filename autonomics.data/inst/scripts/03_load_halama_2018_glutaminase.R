cee0109f9821cfcc69a5da9fc9a9bf31c9e157b6
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-05-17 14:01:54 +0300

    Complete script to load and explore halama 2018 dataset

--- autonomics.data/inst/scripts/03_load_halama_2018_glutaminase.R
+++ autonomics.data/inst/scripts/03_load_halama_2018_glutaminase.R
@@ -1,21 +1,17 @@
 # Load functions
 require(magrittr)
-ggplot2::theme_set(ggplot2::theme_bw())
 
 # Load data
-glutaminase <- autonomics.import::load_metabolon('inst/extdata/glutaminase/glutaminase.xlsx')
+glutaminase <- 'extdata/glutaminase/glutaminase.xlsx'   %>% 
+                system.file(package = 'autonomics.data') %>% 
+                autonomics.import::load_metabolon()
 
 # Rm blank samples
 glutaminase$subgroup
 
 # Explore
+ggplot2::theme_set(ggplot2::theme_bw())
 glutaminase %>% autonomics.explore::plot_projected_samples2(
    method    = c('pca', 'lda', 'sma', 'pls'),
    facet_var = c('pca', 'lda', 'sma', 'pls'),
    nrow = 2)
-
-
-glutaminase %>% autonomics.explore::plot_pca_samples2()
-glutaminase %>% autonomics.explore::plot_sma_samples2()
-glutaminase %>% autonomics.explore::plot_pls_samples2()
-glutaminase %>% autonomics.explore::plot_lda_samples2()
