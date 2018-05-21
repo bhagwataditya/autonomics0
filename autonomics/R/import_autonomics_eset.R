31b82cc67ab1906514079f54b9d1bc5b1747699a
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-05-21 15:35:18 +0300

    Mv functions in import_autonomics_eset.R into import_autonomics_import.R

--- autonomics/R/import_autonomics_eset.R
+++ autonomics/R/import_autonomics_eset.R
@@ -1,55 +1,9 @@
-#' exprs
-#' 
-#' \code{\link[autonomics.import]{fdata}}
-#' @name exprs
-#' @importFrom autonomics.import  exprs   exprs<- 
-#' @export exprs
-#' @export exprs<-
-NULL
 
 
-#' sdata
-#' 
-#' \code{\link[autonomics.import]{sdata}}
-#' @name sdata
-#' @importFrom autonomics.import  sdata   sdata<- 
-#' @export sdata
-#' @export sdata<-
-NULL
 
 
-#' fdata
-#' 
-#' \code{\link[autonomics.import]{fdata}}
-#' @name fdata
-#' @importFrom autonomics.import  fdata   fdata<- 
-#' @export fdata
-#' @export fdata<-
-NULL
 
 
-#' prepro
-#' 
-#' \code{\link[autonomics.import]{prepro}}
-#' @name prepro
-#' @importFrom autonomics.import  prepro
-#' @export prepro
-NULL
 
 
-#' filter_features
-#' 
-#' \code{\link[autonomics.import]{filter_features}}
-#' @name filter_features
-#' @importFrom autonomics.import  filter_features
-#' @export filter_features
-NULL
 
-
-#' filter_samples
-#' 
-#' \code{\link[autonomics.import]{filter_samples}}
-#' @name filter_samples
-#' @importFrom autonomics.import  filter_samples
-#' @export filter_samples
-NULL
--- autonomics/R/import_autonomics_import.R
+++ autonomics/R/import_autonomics_import.R
@@ -7,6 +7,44 @@
 NULL
 
 
+#' exprs
+#' 
+#' \code{\link[autonomics.import]{fdata}}
+#' @name exprs
+#' @importFrom autonomics.import  exprs   exprs<- 
+#' @export exprs
+#' @export exprs<-
+NULL
+
+
+#' fdata
+#' 
+#' \code{\link[autonomics.import]{fdata}}
+#' @name fdata
+#' @importFrom autonomics.import  fdata   fdata<- 
+#' @export fdata
+#' @export fdata<-
+NULL
+
+
+#' filter_features
+#' 
+#' \code{\link[autonomics.import]{filter_features}}
+#' @name filter_features
+#' @importFrom autonomics.import  filter_features
+#' @export filter_features
+NULL
+
+
+#' filter_samples
+#' 
+#' \code{\link[autonomics.import]{filter_samples}}
+#' @name filter_samples
+#' @importFrom autonomics.import  filter_samples
+#' @export filter_samples
+NULL
+
+
 #' Load exiqon data
 #' 
 #' \code{\link[autonomics.import]{load_exiqon}}
@@ -48,6 +86,23 @@ NULL
 #' @name load_metabolon
 #' @importFrom autonomics.import   load_metabolon
 #' @export load_metabolon
+NULL
+
+#' prepro
+#' 
+#' \code{\link[autonomics.import]{prepro}}
+#' @name prepro
+#' @importFrom autonomics.import  prepro
+#' @export prepro
+NULL
+
+#' sdata
+#' 
+#' \code{\link[autonomics.import]{sdata}}
+#' @name sdata
+#' @importFrom autonomics.import  sdata   sdata<- 
+#' @export sdata
+#' @export sdata<-
 NULL
 
 
