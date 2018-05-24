f151c6c102276384827bf1d673a19fb0215a20da
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-05-24 14:15:50 +0300

    Improved plot_venns

--- autonomics.plot/R/plot_venns.R
+++ autonomics.plot/R/plot_venns.R
@@ -14,12 +14,23 @@
 plot_venns <- function(
    x,
    filename = NULL,
-   width = 7,
-   height = 7,
-   title = NULL
+   width    = 7,
+   height   = width,
+   title    = NULL,
+   colors    = autonomics.plot::make_gg_colors(1:length(x))
 ){
    futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
-   p <- VennDiagram::venn.diagram(x, main = title, filename = NULL)
+   colors <- autonomics.plot::make_gg_colors(1:length(x))
+   p <- VennDiagram::venn.diagram(
+      x,
+      main     = title,
+      filename = NULL,
+      fill     = colors,
+      col      = colors,
+      cat.col  = colors,
+      cat.dist = 0.08,
+      main.cex = 1.2,
+      main.pos = c(0.5, 0.97))
    if (!is.null(filename))   grDevices::pdf(filename, width = width, height = height)
    autonomics.support::cdraw(p)
    if (!is.null(filename))   grDevices::dev.off()
