623e50c116411b465298da0a8d852f5089ff3f0c
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-06-05 15:18:38 +0300

    eset_to_*_dt -> sumexp_to_*_dt

--- autonomics.import/R/eset_to_table.R
+++ autonomics.import/R/eset_to_table.R
@@ -1,12 +1,13 @@
-#' Convert eset into wide datatable
-#' @param object eset
-#' @param fid      fvar carrying feature id
-#' @param fvars    additional fvars to include in table
-#' @return datatable
+#' Convert sumexp into wide datatable
+#' @param object sumexp
+#' @param fid    fvar carrying feature id
+#' @param fvars  additional fvars to include in table
+#' @param ...    (backward compatibility)
+#' @return wide data.table
 #' @importFrom data.table  data.table
 #' @importFrom magrittr    %>%
 #' @export
-eset_to_wide_table <- function(
+sumexp_to_wide_dt <- function(
    object,
    fid = autonomics.import::fid_var(object),
    fvars = character(0)
@@ -17,17 +18,24 @@ eset_to_wide_table <- function(
   wide1
 }
 
-#' Convert eset into long datatable
-#' @param object eset
-#' @param fid      fvar carrying feature id
-#' @param fvars    additional fvars to include in table
-#' @param sid      svar carrying sample id
-#' @param svars    additional svars to include in table
-#' @return datatable
+#' @rdname sumexp_to_wide_dt
+eset_to_wide_table <- function(...){
+   .Deprecated('sumexp_to_wide_dt')
+   sumexp_to_wide_dt(...)
+}
+
+#' Convert sumexp into long datatable
+#' @param object sumexp
+#' @param fid    fvar carrying feature id
+#' @param fvars  additional fvars to include in table
+#' @param sid    svar carrying sample id
+#' @param svars  additional svars to include in table
+#' @param ...    (backward compatibility)
+#' @return long data.table
 #' @importFrom data.table   data.table
 #' @importFrom magrittr     %>%
 #' @export
-eset_to_long_table <- function(
+sumexp_to_long_dt <- function(
    object,
    fid = autonomics.import::fid_var(object),
    fvars = character(0),
@@ -36,8 +44,14 @@ eset_to_long_table <- function(
 ){
   sdata1 <- autonomics.import::sdata(object) %>% magrittr::extract(, c('sample_id', svars), drop = FALSE)
   # Note: unique is to avoid duplication of same fields in fid and fvars
-  object %>% autonomics.import::eset_to_wide_table(fid, fvars) %>%
+  object %>% autonomics.import::sumexp_to_wide_dt(fid, fvars) %>%
                data.table::melt.data.table(id.vars = unique(c(fid, fvars)), variable.name = sid, value.name = 'value') %>%
                merge(sdata1, by = sid) %>%
                magrittr::extract(, unique(c(fid, fvars, sid, svars, 'value')), with = FALSE)
+}
+
+#' @rdname sumexp_to_long_dt
+eset_to_long_table <- function(...){
+   .Deprecated('sumexp_to_long_dt')
+   sumexp_to_long_dt(...)
 }
