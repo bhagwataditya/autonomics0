fbcb7ce04fc159906c2549c99aa8dee37f06ebbd
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-06-15 16:43:11 +0300

    Create autonomics.support::pull_columns

---
+++ autonomics.support/R/pull_columns.R
@@ -0,0 +1,19 @@
+#' Pull columns in a dataframe to the front
+#' @param df dataframe
+#' @param selected_cols 
+#' @return dataframe with re-ordered columns
+#' @examples 
+#' require(magrittr)
+#' df <- data.frame(
+#'    symbol = c('A1BG', 'A2M'), 
+#'    id     = c('1',    '2'),
+#'    name   = c('alpha-1-B glycoprotein', 'alpha-2-macroglobulin'), 
+#'    type   = c('proteincoding', 'proteincoding'))
+#' first_cols <- c('id', 'symbol') 
+#' df %>% autonomics.support::pull_columns(first_cols)
+#' @importFrom magrittr %>% 
+#' @export
+pull_columns <- function(df, first_cols){
+  df %>% 
+  magrittr::extract(, c(first_cols, setdiff(names(df), first_cols)), drop = FALSE)
+}
