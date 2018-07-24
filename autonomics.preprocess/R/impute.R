0bebbacf6b02b541fa7a70321f4706049434cf12
Johannes Graumann <github@graumannschaft.org>
2018-07-24 13:33:11 +0200

    Amend imputation to enable reproducible imputation;

--- autonomics.preprocess/R/impute.R
+++ autonomics.preprocess/R/impute.R
@@ -8,12 +8,27 @@
 #' See \href{https://www.biorxiv.org/content/early/2017/08/17/171967}{Wei et al. 2017}
 #'
 #' @param object SummarizedExperiment, eSet, or EList
+#' @param random_seed Integer used with \code{\link[R.utils]{withSeed}} for reproducible
+#' imputation (the used \code{\link[imputeLCMD]{impute.QRILC}} implys random draws from a distribution)
 #' @return updated object
 #' @importFrom magrittr %<>% %>%
 #' @export
-impute <- function(object){
-   autonomics.import::exprs(object) %<>% imputeLCMD::impute.QRILC() %>%
-                                       magrittr::extract2(1)
+impute <- function(object, random_seed = NULL){
+   if(is.null(random_seed))
+   {
+     autonomics.import::exprs(object) %<>%
+       imputeLCMD::impute.QRILC() %>%
+       magrittr::extract2(1)
+   } else {
+     assertive.types::assert_is_an_integer(random_seed)
+     autonomics.import::exprs(object) <- R.utils::withSeed(
+       { 
+         autonomics.import::exprs(object) %>%
+           imputeLCMD::impute.QRILC() %>%
+           magrittr::extract2(1)
+       },
+       seed = random_seed)
+   }
    object
 }
 
