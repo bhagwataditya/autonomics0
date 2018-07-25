3d4f6f19cd3320b25bf1a71f1527fada44ad050f
Johannes Graumann <github@graumannschaft.org>
2018-07-25 12:17:50 +0200

    Expand impute to accept several methodologies;

--- autonomics.preprocess/R/impute.R
+++ autonomics.preprocess/R/impute.R
@@ -5,31 +5,91 @@
 #' They are well imputed with the QRILC (quantile regression imputation of left censored data).
 #'
 #' See \href{https://cran.r-project.org/package=imputeLCMD}{imputeLCMD package on CRAN}
-#' See \href{https://www.biorxiv.org/content/early/2017/08/17/171967}{Wei et al. 2017}
 #'
 #' @param object SummarizedExperiment, eSet, or EList
+#' @param method Imputation method employed. See Section 'Details'.
 #' @param random_seed Integer used with \code{\link[R.utils]{withSeed}} for reproducible
 #' imputation (the used \code{\link[imputeLCMD]{impute.QRILC}} implys random draws from a distribution)
+#' @param ... Parameters handed on to the imputation methods.
+#' @details The function under the performs no imutation if \code{method = 'none'}, uses
+#'  \code{\link[missForest]{missForest}} if \code{method = 'missForest'} and the parameter-named
+#'  methods from \pkg{imputeLCMD} in all other cases.
 #' @return updated object
+#' @importFrom imputeLCMD impute.MinDet impute.MinProb impute.QRILC
 #' @importFrom magrittr %<>% %>%
+#' @importFrom missForest missForest
 #' @export
-impute <- function(object, random_seed = NULL){
-   if(is.null(random_seed))
-   {
-     autonomics.import::exprs(object) %<>%
-       imputeLCMD::impute.QRILC() %>%
-       magrittr::extract2(1)
-   } else {
-     assertive.types::assert_is_an_integer(random_seed)
-     autonomics.import::exprs(object) <- R.utils::withSeed(
-       { 
-         autonomics.import::exprs(object) %>%
-           imputeLCMD::impute.QRILC() %>%
-           magrittr::extract2(1)
-       },
-       seed = random_seed)
-   }
-   object
+impute <- function(
+  object,
+  method = c('none', 'missForest', 'impute.minDet', 'impute.minProb', 'impute.QRILC'),
+  random_seed = NULL,
+  ...)
+{
+# Check prerequisites -----------------------------------------------------
+  autonomics.import::assert_is_valid_eset(object)
+  method %<>%
+    match.arg(
+      choices = c('none', 'missForest', 'impute.MinDet', 'impute.MinProb', 'impute.QRILC'),
+      several.ok = FALSE)
+  if(!is.null(random_seed))
+  {
+    assertive.types::assert_is_a_number(random_seed)
+  }
+
+# Processing --------------------------------------------------------------
+  # Eject if nothing is to be done
+  if(method == 'none')
+  {
+    return(object)
+  }
+  
+  # Retrieve the function to be used
+  # https://stackoverflow.com/a/10022480/2103880
+  pkg <- method %>%
+    switch(
+      missForest = 'missForest',
+      'imputeLCMD')
+  fn <- get(method, asNamespace(pkg))
+  
+  largs <- list(autonomics.import::exprs(object)) %>%
+    magrittr::set_names(
+      method %>%
+        switch(
+          missForest = 'xmis',
+          'dataSet.mvs'))
+  
+  # Execute dependent on 'random_seed'
+  ## Capture output is currently needed, as imputeLCMD::impute.MinProb contains a spurious print statement (v.2.0)
+  if(is.null(random_seed))
+  {
+    utils::capture.output(
+      impute_result <- do.call(fn, largs, ...))
+  } else {
+    utils::capture.output(
+      impute_result <- R.utils::withSeed(
+        { do.call(fn, autonomics.import::exprs(object), ...) },
+        seed = random_seed))
+  }
+  
+  # Extract dependent on method
+  # missForest --> list[['ximp']]
+  # MinDet --> matrix
+  # MinProb --> matrix
+  # QRILC --> list[[1]]
+  index <- method %>%
+    switch(
+      missForest   = 'ximp',
+      impute.QRILC = 1,
+      NULL)
+  if(!is.null(index))
+  {
+    impute_result %<>%
+      magrittr::extract2(index)
+  }
+  
+  # Reassemble & Return
+  autonomics.import::exprs(object) <- impute_result
+  return(object)
 }
 
 
