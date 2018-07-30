afe576352d503b7731b07992953b5b59bf332e17
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-07-30 13:14:09 +0300

    autonomics.support:: create rm_empty_vars()

---
+++ autonomics.support/R/rm_empty_vars.R
@@ -0,0 +1,17 @@
+
+#' rm empty vars from list
+#' @param x list, dataframe, etc.
+#' @return list without empty vars
+#' @importFrom magrittr %>%
+#' @export
+rm_empty_vars <- function(x){
+  assertive.types::assert_is_list(x)
+
+  for (curvar in names(x)){
+    isempty <- x[[curvar]] %>% assertive.strings::is_missing_or_empty_character() %>% all()
+    if (isempty){
+      x[[curvar]] <- NULL
+    }
+  }
+  x
+}
