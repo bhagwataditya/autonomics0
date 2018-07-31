5a20ca23531a752637056c3ffe8363eba23efd8b
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-07-31 13:41:13 +0300

    Enhance load_go_genes doc

--- autonomics.annotate/R/load_go_genes.R
+++ autonomics.annotate/R/load_go_genes.R
@@ -1,15 +1,15 @@
 #' Load GO genes
 #' @param goid       GO ID
 #' @param organism  'Homo sapiens'
-#' @param gene_id   'ENTREZID', 'GENENAME'
+#' @param gene_id   'ENTREZID', 'ENSEMBL', 'UNIPROT', 'SYMBOL', 'GENENAME'
 #' @return character vector
 #' @examples
 #' require(magrittr)
 #' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'ENTREZID') %>% head()
 #' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'ENSEMBL')  %>% head()
+#' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'UNIPROT') %>% head()
 #' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'SYMBOL') %>% head()
 #' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'GENENAME') %>% head()
-#' load_go_genes(goid = 'GO:0006954', organism = 'Homo sapiens', gene = 'UNIPROT') %>% head()
 #' @importFrom magrittr %>%
 #' @export
 load_go_genes <- function(goid, organism, gene = 'ENTREZID'){
