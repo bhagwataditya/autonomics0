6ecf3fc9bbdfdc23d05c6c7792c8e25760fdf5d6
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-08-13 14:17:23 +0300

    Create split_components(subgroup_values), and use it in scomponents() and subgroup_components()

--- autonomics.import/R/scomponents.R
+++ autonomics.import/R/scomponents.R
@@ -1,54 +1,82 @@
 
 
-#' Get subgroup components
+#' Split (subgroup) components
 #'
 #' Get data.table in which subgroup components have been decomposed
 #'
-#' @param object  SummarizedExperiment, eSet, or EList
-#' @param svar    sample var
+#' @param values character vector with subgroup values
+#' @param sep character(1): separator
 #' @return data.table with decomposed subgroup components
 #' @examples
 #' require(magrittr)
 #' if (require(billing.differentiation.data)){
-#'    billing.differentiation.data::protein.ratios %>% scomponents('subgroup')
-#'    billing.differentiation.data::protein.ratios %>% subgroup_components()
+#'    object <- billing.differentiation.data::protein.ratios
+#'    object %>% subgroup_values() %>% split_components()
 #' }
 #' if (require(autonomics.data)){
-#'    file <- system.file('extdata/glutaminase/glutaminase.xlsx',
-#'                         package = 'autonomics.data')
-#'    object <- autonomics.import::load_metabolon(file)
-#'    object  %>% scomponents('subgroup')
-#'    object  %>% subgroup_components()
+#'    autonomics.data::glutaminase %>% subgroup_values()  %>% split_components()
 #' }
 #' if (require(subramanian.2016)){
-#'    subramanian.2016::metabolon    %>% scomponents('subgroup')
-#'    subramanian.2016::metabolon    %>% subgroup_components()
+#'    subramanian.2016::metabolon %>% subgroup_values()   %>% split_components()
 #' }
 #' if (require(graumann.lfq)){
-#'    graumann.lfq::lfq.intensities  %>% scomponents('subgroup')
-#'    graumann.lfq::lfq.intensities  %>% subgroup_components()
+#'    graumann.lfq::lfq.intensities %>% subgroup_values() %>% split_components()
 #' }
 #' @importFrom magrittr %>%
 #' @export
-scomponents <- function(object, svar){
-
-   # Extract
-   svar_levels <- object %>% autonomics.import::slevels(svar)
-   sep         <- object %>% autonomics.import::ssep(svar)
+split_components <- function(
+   values,
+   sep = autonomics.import::infer_design_sep(values)
+){
 
    # Single component
-   if (is.null(sep)) return(data.table::data.table(V1 = svar_levels))
+   if (is.null(sep)) return(data.table::data.table(V1 = values))
 
    # Multiple components
-   y <- svar_levels %>% stringi::stri_split_fixed(sep)
+   y <- values %>% stringi::stri_split_fixed(sep)
    n.component <- length(y[[1]])
    1:n.component %>% lapply(function(z) vapply(y, magrittr::extract, character(1), z)) %>%
       data.table::as.data.table()
 }
 
-#' @rdname scomponents
+#' Split (subgroup) components
+#'
+#' Get data.table in which subgroup components have been decomposed
+#'
+#' @param object SummarizedExperiment
+#' @param svar character
+#' @return data.table with decomposed subgroup components
+#' @examples
+#' require(magrittr)
+#' if (require(billing.differentiation.data)){
+#'    object <- billing.differentiation.data::protein.ratios
+#'    object %>% subgroup_components()
+#'    object %>% scomponents('subgroup')
+#' }
+#' if (require(autonomics.data)){
+#'    object <- autonomics.data::glutaminase
+#'    object %>% subgroup_components()
+#'    object %>% scomponents('subgroup')
+#' }
+#' if (require(subramanian.2016)){
+#'    object <- subramanian.2016::metabolon
+#'    object %>% subgroup_components()
+#'    object %>% scomponents('subgroup')
+#' }
+#' if (require(graumann.lfq)){
+#'    object <- graumann.lfq::lfq.intensities
+#'    object %>% subgroup_components()
+#'    object %>% scomponents('subgroup')
+#' }
 #' @importFrom magrittr %>%
 #' @export
 subgroup_components <- function(object){
-   object %>% scomponents('subgroup')
+   object %>% autonomics.import::subgroup_values() %>% autonomics.import::split_components()
+}
+
+#' @rdname subgroup_components
+#' @importFrom magrittr %>%
+#' @export
+scomponents <- function(object, svar){
+   object %>% autonomics.import::svalues(svar) %>% autonomics.import::split_components()
 }
