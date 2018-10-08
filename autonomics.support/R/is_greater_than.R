1ff84a8002bcb5c342d8b2ec813f240370f28e88
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-10-08 14:53:18 +0300

    Create is_greater_than(), is_weakly_greater_than() etc.

---
+++ autonomics.support/R/is_greater_than.R
@@ -0,0 +1,34 @@
+#' NA-accepting versions of comparator operators
+#' @param a numeric scalar, vector, or matrix
+#' @param b numeric scalar, vector, or matrix
+#' @return logical scalar, vector, or matrix
+#' @importFrom magrittr %>%
+#' @export
+is_greater_than <- function(a,b){
+  (a > b) %>%
+    (function(x){ x[is.na(x)] <- FALSE; x})
+}
+
+#' @rdname is_greater_than
+#' @importFrom magrittr %>%
+#' @export
+is_weakly_greater_than <- function(a,b){
+   (a >= b) %>%
+   (function(x){ x[is.na(x)] <- FALSE; x})
+}
+
+#' @rdname is_greater_than
+#' @importFrom magrittr %>%
+#' @export
+is_less_than <- function(a,b){
+  (a < b) %>%
+  (function(x){ x[is.na(x)] <- FALSE; x})
+}
+
+#' @rdname is_greater_than
+#' @importFrom magrittr %>%
+#' @export
+is_weakly_less_than <- function(a,b){
+  (a <= b) %>%
+  (function(x){ x[is.na(x)] <- FALSE; x})
+}
