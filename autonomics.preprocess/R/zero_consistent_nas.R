6cc71dd7f9fa3fa768a497b681dc2c7fa1edd1d4
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-10-09 16:11:52 +0300

    Create zero_consistent_nas() to complement na_inconsistent_zeroes()

--- autonomics.preprocess/R/zero_consistent_nas.R
+++ autonomics.preprocess/R/zero_consistent_nas.R
@@ -1,29 +1,17 @@
-#' @rdname missify_inconsistent_zeroes
+#' @rdname zero_consistent_nas
 #' @export
 zero_to_na_if_not_all_zero <- function(
    no_zero = c('fail', 'warn_passthrough', 'passthrough')[1],
    ...
 ){
-   missify_inconsistent_zeroes(...)
+   na_inconsistent_zeroes(...)
 }
 
-#' Missify inconsistent zeroes
-#'
-#' Convert inconsistent zeroes into NAs. \cr
-#' Inconsistent zeroes are those not replicated across samples of the same subgroup.
-#' Such zeroes are likely caused by identification/quantification stochasticity.
-#' Their proper representation is NA rather than zero.
-#' Failure to make this distinction diminishes statistical power.
-#'
-#' Inconsistent zeroes are common in LCMS proteomics and metabolomics, due to the
-#' stochastic nature of the identification process.
-#'
-#' @param object SummarizedExperiment
-#' @param no_zero \code{\link{character}} defining how to react of there are no \code{0}s (zeros) present in the data set
-#' @return updated object
+
+#' @rdname zero_consistent_nas
 #' @importFrom magrittr %<>% %>%
 #' @export
-missify_inconsistent_zeroes <- function(
+na_inconsistent_zeroes <- function(
   object,
   verbose = FALSE
 ){
@@ -116,3 +104,58 @@ missify_inconsistent_zeroes <- function(
   if (verbose)  autonomics.support::cmessage('\t\tConvert inconsistent zeroes into NAs')
   return(object)
 }
+
+
+#' NA inconsistent zeroes. Zero consistent NAS.
+#'
+#' Convert consistent NAS into zeroes.
+#' And convert inconsistent zeroes into NAS.
+#'
+#' Inconsistent zeroes are those not replicated across samples of the same subgroup.
+#' Such zeroes are likely caused by identification/quantification stochasticity.
+#' Their proper representation is NA rather than zero.
+#' Failure to make this distinction diminishes statistical power.
+#'
+#' Inconsistent zeroes are common in LCMS proteomics and metabolomics, due to the
+#' stochastic nature of the identification process.
+#'
+#' @param object SummarizedExperiment
+#' @param no_zero \code{\link{character}} defining how to react of there are no \code{0}s (zeros) present in the data set
+#' @return updated object
+#' @examples
+#' if (require(subramanian.2016)){
+#'    object <- system.file('extdata/exiqon/subramanian.2016.exiqon.xlsx',
+#'                           package = 'subramanian.2016') %>%
+#'              autonomics.import::load_exiqon(infer_design_from_sampleids = TRUE)
+#' }
+#' @return updated object
+#' @importFrom magrittr %>%
+#' @export
+zero_consistent_nas <- function(object, verbose = FALSE){
+
+   # Melt
+   dt <- object %>% autonomics.import::sumexp_to_long_dt(svars = 'subgroup')
+
+   # Zero consistent NAs
+   dt %>% magrittr::extract(, consistent_na := all(is.na(value)), by = c('feature_id', 'subgroup')) %>%
+          magrittr::extract(consistent_na==TRUE, value := 0)
+
+   # Report
+   n_consistentfeature  <- dt[consistent_na==TRUE, length(unique(feature_id))]
+   n_consistentsubgroup <- dt[consistent_na==TRUE, length(unique(subgroup  ))]
+   n_subgroup           <- dt[, length(unique(subgroup  ))]
+   if (verbose) autonomics.support::cmessage("Nullify consistent NAS for %d/%d subgroups in %d/%d features",
+                                             n_consistentsubgroup, n_subgroup, n_consistentfeature,  nrow(object))
+
+   # Cast into exprs
+   autonomics.import::exprs(object) <- dt %>% data.table::dcast.data.table(feature_id ~ sample_id, value.var = 'value') %>%
+                                              data.table::setkey(feature_id)      %>%
+                                              magrittr::extract(rownames(object)) %>%
+                                             (function(x)  x[, -1, with = FALSE]  %>%
+                                                           data.matrix()          %>%
+                                                           magrittr::set_rownames(x[[1]]) )
+   # Return
+   object
+}
+
+
