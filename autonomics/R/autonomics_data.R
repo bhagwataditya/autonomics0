839a1cc8980d76a2b807ce4c6b74a7dd7d69c5b6
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-10-30 13:34:13 +0300

    Export glutaminase inhibitor dataset

---
+++ autonomics/R/autonomics_data.R
@@ -0,0 +1,7 @@
+#' Halama et al. (2018) glutaminase inhibitor dataset
+#' 
+#' \code{\link[autonomics.data]{glutaminase}}
+#' @name glutaminase
+#' @importFrom  autonomics.data   glutaminase
+#' @export  glutaminase
+NULL
