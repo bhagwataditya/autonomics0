3979e23207a8de45ba1e5dd9fe0ba10a9e6f3613
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2018-11-29 14:09:26 +0300

    rm_empty_vars: fix

--- autonomics.support/R/rm_empty_vars.R
+++ autonomics.support/R/rm_empty_vars.R
@@ -8,7 +8,7 @@ rm_empty_vars <- function(x){
   assertive.types::assert_is_list(x)
 
   for (curvar in names(x)){
-    isempty <- x[[curvar]] %>% assertive.strings::is_missing_or_empty_character() %>% all()
+    isempty <- x[[curvar]] %>% as.character() %>% assertive.strings::is_missing_or_empty_character() %>% all()
     if (isempty){
       x[[curvar]] <- NULL
     }
