c67bab69809a298d6e2116d81cd621d2be7b0f7d
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2019-01-29 16:20:07 +0300

    Move guess_sep() and guess_subgroup() to guess.R, and S3ify both functions.

---
+++ autonomics.import/R/guess.R
@@ -0,0 +1,197 @@
+#===================================================================================================
+#' Guess separator
+#' @param x                   character vector or SummarizedExperiment
+#' @param svar                string
+#' @param possible_separators character vector with possible separators to look for
+#' @param verbose             logical
+#' @return separator (string) or NULL (if no separator could be identified)
+#' @examples
+#' require(magrittr)
+#'
+#' x <- c('PERM_NON.R1[H/L]', 'PERM_NON.R2[H/L]', 'PERM_NON.R3[H/L]', 'PERM_NON.R4[H/L]')
+#' x %>% guess_sep()
+#'
+#' x <- c('WT untreated 1', 'WT untreated 2', 'WT treated 1')
+#' x %>% guess_sep()
+#'
+#' x <- c('group1', 'group2', 'group3.R1')
+#' x %>% guess_sep()
+#'
+#' if (require(autonomics.data))   autonomics.data::glutaminase %>%
+#'                                 autonomics.import::guess_sep()
+#'
+#' if (require(autonomics.data))   autonomics.data::stemcomp.proteinratios %>%
+#'                                 autonomics.import::guess_sep()
+#'
+#' if (require(subramanian.2016))  subramanian.2016::metabolon  %>%
+#'                                 autonomics.import::guess_sep()
+#'
+#' if (require(graumann.lfq))      graumann.lfq::lfq.intensities %>%
+#'                                 autonomics.import::guess_sep()
+#' @export
+guess_sep <- function (x, ...) {
+   UseMethod("guess_sep", x)
+}
+
+
+#' @rdname guess_sep
+#' @importFrom magrittr %>%
+#' @export
+guess_sep.character <- function(
+   x,
+   possible_separators = c('.', ' ', '_'),
+   verbose = FALSE
+){
+   . <- NULL
+   sep_freqs <- Map(function(y) stringi::stri_split_fixed(x, y), possible_separators)        %>%
+                lapply(function(y) y %>% vapply(length, integer(1)))                                  %>%
+                magrittr::extract( vapply(., autonomics.support::has_identical_values, logical(1)))   %>%
+                vapply(unique, integer(1))
+
+   # No separator detected - return NULL
+   if (all(sep_freqs==1)){
+      if (verbose)  autonomics.support::cmessage('%s: no (consistent) separator. Returning NULL', x[1])
+      return(NULL)   # no separator detected
+   }
+
+   # Find best separator
+   best_sep <- sep_freqs %>%
+               magrittr::extract(.!=1)  %>%
+               magrittr::extract(autonomics.support::is_max(vapply(., magrittr::extract, integer(1), 1)))   %>%
+               names()
+
+   # Ambiguous separator - take first from tail
+   if (length(best_sep)>1){
+      pattern <- best_sep %>% paste0(collapse='') %>% paste0('[', ., ']')
+      best_sep <- x[1] %>% stringi::stri_extract_last_regex(pattern)
+   }
+
+   # Separator identified - return
+   if (verbose) autonomics.support::cmessage("\t\tGuess sep: '%s'", best_sep)
+   return(best_sep)
+}
+
+
+#' @rdname guess_sep
+#' @importFrom magrittr %>%
+#' @export
+guess_sep.SummarizedExperiment <- function(
+   x,
+   svar = 'sample_id',
+   possible_separators = c('.', '_', ' '),# if (autonomics.import::contains_ratios(x)) c('.', ' ') else c('.', '_', ' '),
+   verbose = FALSE
+){
+   x %>%
+   autonomics.import::svalues(svar) %>%
+   guess_sep(possible_separators = possible_separators,
+             verbose             = verbose)
+}
+
+infer_design_sep <- function(...){
+   .Deprecated('guess_sep')
+   guess_sep(...)
+}
+
+#' @rdname guess_sep
+#' @importFrom magrittr %>%
+#' @export
+ssep <- function(...){
+   .Deprecated('guess_sep')
+   guess_sep(...)
+}
+
+#' @rdname guess_sep
+#' @importFrom  magrittr %>%
+#' @export
+subgroup_sep <- function(...){
+   .Deprecated('guess_sep')
+   guess_sep(...)
+}
+
+
+
+#=================================================================================
+#' Guess subgroup values
+#' @param x             charactervector, SummarizedExperiment
+#' @param sep           character(1)
+#' @param verbose       logical(1)
+#' @return character(n)
+#' @examples
+#' require(magrittr)
+#'
+#' # charactervector
+#'    # No sep: subgroup = x
+#'       x <- c("EM00", "EM01", "EM02")
+#'       x %>% guess_subgroup_values()
+#'
+#'    # Sep: subgroup = head components of x
+#'       x <- c("UT_10h_R1", "UT_10h_R2", "UT_10h_R3")
+#'       x %>% guess_subgroup_values()
+#'
+#'       x <- c("EM00_STD.R1", "EM01_STD.R1", "EM01_EM00.R1")
+#'       x %>% guess_subgroup_values()
+#'
+#' @export
+guess_subgroup_values <- function (x, ...) {
+   UseMethod("guess_subgroup_values", x)
+}
+
+#' @rdname guess_subgroup_values
+#' @importFrom magrittr %>%
+#' @export
+guess_subgroup_values.character <- function(
+   x,
+   sep     = x %>% autonomics.import::guess_sep(),
+   verbose = FALSE
+){
+   # Guess
+   subgroup_values <- if (is.null(sep)){ x
+                      } else {           x %>% stringi::stri_split_fixed(sep) %>%
+                                               vapply(function(y) y %>%
+                                                                  magrittr::extract(1:(length(y)-1)) %>%
+                                                                  paste0(collapse = sep), character(1))
+                      }
+   # Inform
+   if (verbose)   autonomics.support::cmessage('\t\tGuess subgroup values: %s => %s', x[1], subgroup_values[1])
+
+   # Return
+   return(subgroup_values)
+}
+
+#' @rdname guess_subgroup_values
+#' @importFrom magrittr %>%
+#' @export
+guess_subgroup_values.SummarizedExperiment <- function(
+   x,
+   sep          = x %>% autonomics.import::guess_sep(),
+   verbose      = FALSE
+){
+
+   # already in object
+   if ('subgroup' %in% autonomics.import::svars(object)){
+      if (verbose) autonomics.support::cmessage("\t\tUse 'subgroup' values in object ")
+      return(autonomics.import::sdata(object)$subgroup)
+   }
+
+   # guess from sampleid values
+   if (verbose) autonomics.support::cmessage("\t\tGuess 'subgroup' from 'sample_id' values")
+   x %>% autonomics.import::sampleid_values() %>%
+         autonomics.import:::guess_subgroup_values.character(verbose = verbose)
+}
+
+
+#==========================================================
+
+# guess_subject_values <- function (x, ...) {
+#    UseMethod("guess_subject_values", x)
+# }
+#
+# guess_subject_values.character(
+#    x,
+#    sep     = autonomics.import::guess_sep(x),
+#    verbose = FALSE
+# ){
+#    NULL
+# }
+
+
