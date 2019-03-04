37bf5b700cc2b80309c508443b954a14f68da32e
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2019-03-04 13:24:57 +0300

    Recreate updated datasets in autonomics.data

--- autonomics.data/inst/scripts/save_datasets.R
+++ autonomics.data/inst/scripts/save_datasets.R
@@ -5,12 +5,10 @@ require(magrittr)
 
 # PROTEINGROUPS
 stemcomp.proteinratios <- 'extdata/stemcomp/maxquant/proteinGroups.txt' %>% 
-                           system.file(package = 'autonomics.data') %>% 
-                           autonomics.import::load_proteingroups(infer_design_from_sampleids = TRUE) %>% 
-                           autonomics.preprocess::invert_ratios(
-                              invert_subgroups = c('E_EM', 'E_BM', 'EM_BM'), 
-                              subgroup_frac = '_') %>% 
-                           autonomics.import::deconvolute_proteingroups('../data/uniprot_hsa_20140515.fasta', drop_isoform_info = TRUE) %>% 
+                           system.file(package = 'autonomics.data')     %>% 
+                           autonomics.import::read_proteingroups()      %>% 
+                           autonomics::prepare_proteingroups(invert_subgroups = c('E_EM', 'E_BM', 'EM_BM'), 
+                                                             deconvolution_fastafile = '../data/uniprot_hsa_20140515.fasta') %>% 
                            autonomics.import::set_contrastdefs(c( EM_E =  'EM_E', BM_E =  'BM_E', BM_EM = 'BM_EM')) %>% 
                            autonomics.find::add_limma()
 save(stemcomp.proteinratios, file = 'data/stemcomp.proteinratios.RData', compress = 'xz')
@@ -18,7 +16,8 @@ save(stemcomp.proteinratios, file = 'data/stemcomp.proteinratios.RData', compres
 # SOMA
 stemcomp.soma <- 'extdata/stemcomp/soma/stemcomp.adat'      %>% 
                   system.file(package = 'autonomics.data')  %>%
-                  autonomics.import::load_soma()            %>%
+                  autonomics.import::read_somascan()        %>%
+                  autonomics::prepare_somascan()     %>% 
                   autonomics.find::add_limma(c(EM_E  = 'EM-E', BM_E  = 'BM-E', BM_EM = 'BM-EM'))
 save(stemcomp.soma, file = 'data/stemcomp.soma.RData', compress = 'xz')
 
@@ -29,7 +28,8 @@ save(stemcomp.soma, file = 'data/stemcomp.soma.RData', compress = 'xz')
 # PROTEINGROUPS
 stemdiff.proteinratios <- 'extdata/stemdiff/maxquant/proteinGroups.txt' %>% 
                            system.file(package = 'autonomics.data')     %>% 
-                           autonomics.import::load_proteingroups(infer_design_from_sampleids = TRUE)           %>% 
+                           autonomics.import::read_proteingroups()      %>% 
+                           autonomics::prepare_proteingroups(deconvolution_fastafile = '../data/uniprot_hsa_20140515.fasta') %>% 
                            autonomics.import::filter_samples(subgroup %>% stringi::stri_detect_fixed('_STD'))  %>% 
                               # ony interested in ./STD(L) ratios
                            autonomics.import::filter_samples(subgroup %>% stringi::stri_detect_fixed('BLANK_') %>% magrittr::not()) %>% 
@@ -42,12 +42,11 @@ stemdiff.proteinratios <- 'extdata/stemdiff/maxquant/proteinGroups.txt' %>%
                                        x %<>% autonomics.import::arrange_samples(subgroup)
                                        x
                            }) %>% 
-                           autonomics.import::deconvolute_proteingroups('../data/uniprot_hsa_20140515.fasta', drop_isoform_info = TRUE) %>% 
                            autonomics.import::set_contrastdefs(autonomics.find::make_ref_contrasts(.)) %>% 
                            autonomics.find::add_limma()
 
 save(stemdiff.proteinratios, file = 'data/stemdiff.proteinratios.RData', compress = 'xz')
-stemdiff.proteinratios %>% autonomics.explore::plot_pca_samples()
+stemdiff.proteinratios %>% autonomics.plot::plot_pca_samples()
 stemdiff.proteinratios %>% autonomics.plot::plot_sample_distributions()
 stemdiff.proteinratios %>% autonomics.plot::default_color_values(color_var = 'subgroup')
 
@@ -56,13 +55,14 @@ stemdiff.proteinratios %>% autonomics.plot::default_color_values(color_var = 'su
 require(magrittr)
 glutaminase <- 'extdata/glutaminase/glutaminase.xlsx'     %>% 
                 system.file(package = 'autonomics.data')  %>% 
-                autonomics.import::load_metabolon()       #%>% 
+                autonomics.import::read_metabolon()       %>% 
+                autonomics::prepare_metabolon()
                 #autonomics.import::set_contrastdefs(autonomics.find::make_ref_contrasts(.)) %>% 
                 #autonomics.find::add_limma()
 autonomics.import::sdata(glutaminase) %>% str()
 glutaminase$subgroup %<>% factor(autonomics.support::vsprintf('%s_%s', c('UT', 'Veh', 'uM05', 'uM10'), 
                                                                        c('h10', 'h24', 'h48', 'h72')))
-devtools::use_data(glutaminase, compress = 'xz', overwrite = TRUE)
+usethis::use_data(glutaminase, compress = 'xz', overwrite = TRUE)
 
 
 #' Save eset with preprocessed Billing 2016 data
