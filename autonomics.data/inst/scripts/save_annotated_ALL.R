c88517aeb213a68574d0cfad9f38c5ef0028ecdb
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2019-03-05 16:36:37 +0300

    Update ALL dataset

--- autonomics.data/inst/scripts/save_annotated_ALL.R
+++ autonomics.data/inst/scripts/save_annotated_ALL.R
@@ -9,20 +9,21 @@
 save_annotated_ALL <- function(){
    
    getFirstFeatureFromALL <- function(envir){
-      AnnotationDbi::mget(Biobase::featureNames(e$ALL), envir) %>% 
-         sapply(getElement, 1) %>% 
-         unname()
+      AnnotationDbi::mget(autonomics.import::fnames(e$ALL), envir) %>% 
+      sapply(getElement, 1) %>% 
+      unname()
    }
    
    e <- new.env()
    utils::data(list = 'ALL', package = 'ALL', envir = e)
-   Biobase::fData(e$ALL) <- data.frame(
+   e$ALL %<>% SummarizedExperiment::SummarizedExperiment()
+   autonomics.import::fdata(e$ALL) <- data.frame(
       gene_symbols       = getFirstFeatureFromALL(hgu95av2.db::hgu95av2SYMBOL),
       gene_names         = getFirstFeatureFromALL(hgu95av2.db::hgu95av2GENENAME),
       ensembl_gene_ids   = getFirstFeatureFromALL(hgu95av2.db::hgu95av2ENSEMBL),
       entrez_gene_ids    = getFirstFeatureFromALL(hgu95av2.db::hgu95av2ENTREZID),
       uniprot_accessions = getFirstFeatureFromALL(hgu95av2.db::hgu95av2UNIPROT),
-      row.names = Biobase::featureNames(e$ALL), 
+      row.names          = autonomics.import::fnames(e$ALL), 
       stringsAsFactors = FALSE
    )
    
@@ -31,22 +32,14 @@ save_annotated_ALL <- function(){
    e$ALL$subgroup <- paste0(gsub('([BT])[1234]?', '\\1', e$ALL$BT), e$ALL$sex)   # add var subgroup
    
    # Prepare fData
-   Biobase::fData(e$ALL)$feature_id <- rownames(Biobase::fData(e$ALL))
-   e$ALL <- e$ALL[!is.na(Biobase::fData(e$ALL)$gene_symbols), ]                  # rm features with NA gene symbol
-   e$ALL <- e$ALL[!duplicated(Biobase::fData(e$ALL)$gene_symbols), ]             # rm features with duplicate gene symbols
+   autonomics.import::fdata(e$ALL)$feature_id <- rownames(autonomics.import::fdata(e$ALL))
+   e$ALL <- e$ALL[!is.na(autonomics.import::fdata(e$ALL)$gene_symbols), ]                  # rm features with NA gene symbol
+   e$ALL <- e$ALL[!duplicated(autonomics.import::fdata(e$ALL)$gene_symbols), ]             # rm features with duplicate gene symbols
    
-   # Add preprocessing info
-   autonomics.import::prepro(e$ALL) <- autonomics.import::create_prepro_list(
-                                          assay     = 'microarray',
-                                          entity    = 'rna',
-                                          quantity  = 'abundance', 
-                                          software  = 'affymetrix', 
-                                          parameters = list())
-
    # Check and save
    ALL <- e$ALL
    autonomics.import::assert_is_valid_object(ALL)
-   save(ALL, file = 'data/ALL.RData', compress = TRUE)
+   usethis::use_data(ALL, compress = 'xz', overwrite = TRUE)
  }
  
 require(magrittr)
