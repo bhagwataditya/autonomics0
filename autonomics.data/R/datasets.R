5b2018cce1c43e1362f6f97e21025353e12c7311
Aditya Bhagwat <adb2018@qatar-med.cornell.edu>
2019-03-25 08:58:08 +0300

    autonomics.data checks 0 - 1 phospho (STY)Sites.txt - 0

---
+++ autonomics.data/R/datasets.R
@@ -0,0 +1,28 @@
+
+#' Billing et al. (2016) stem cell comparison: protein ratios (sample versus internal standard)
+#' 
+#' @references http://www.nature.com/articles/srep21507
+"stemcomp.proteinratios"
+
+
+#' Billing et al. (2016) stem cell comparison: somascan intensities
+#' 
+#' @references http://www.nature.com/articles/srep21507
+"stemcomp.soma"
+
+
+
+#' Halama et al. (2018) glutaminase inhibitor dataset
+#' @references https://doi.org/10.1016/j.canlet.2018.05.017
+"glutaminase"
+
+
+
+#' Ritz laboratory's ALL data
+#' 
+#' This is a clean and annotated version of the well-known \code{\link[ALL]{ALL}}
+#' dataset from the Bioconductor package 'ALL' (which in its recent version 
+#' doesn't contain feature annotations).
+#' 
+#' @references http://bioconductor.org/packages/release/data/experiment/html/ALL.html
+"ALL"
--- autonomics.data/inst/scripts/save_datasets.R
+++ autonomics.data/inst/scripts/save_datasets.R
@@ -63,25 +63,3 @@ glutaminase$subgroup %<>% factor(autonomics.support::vsprintf('%s_%s', c('UT', '
 usethis::use_data(glutaminase, compress = 'xz', overwrite = TRUE)
 
 
-#' Save eset with preprocessed Billing 2016 data
-#' 
-#' Billing 2016 is a stem cell comparison experiment, 
-#' in which three different types of stem cells (E, EM, BM)
-#' are compared to each other.
-#' 
-#' @noRd
-#' @importFrom magrittr %<>%
-save_billing2016 <- function(){
-   autonomics.import::create_maxquant_sample_file( 'inst/extdata/billing2016/proteinGroups.txt')
-   billing2016 <- autonomics.import::load_proteingroups('inst/extdata/billing2016/proteinGroups.txt') %>% 
-      autonomics.preprocess::invert_ratios(
-         invert_subgroups = c('E_EM', 'E_BM', 'EM_BM'), 
-         subgroup_frac    = '_')
-   billing2016 %<>% autonomics.import::annotate_proteingroups()
-   autonomics.import::fdata(billing2016)$`Fasta headers` <- NULL
-   billing2016 %>%  autonomics.import::fdata() %>% head()
-   billing2016 %<>% autonomics.find::add_limma_to_fdata()
-   save(billing2016, file = 'data/billing2016.RData', compress = 'xz')
-}
-
-#save_billing2016()
