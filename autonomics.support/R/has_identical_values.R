896318ff16299a007d30c6f61c668875fb8db39f
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-07-16 17:39:52 +0200

    rm ununsed function all_have_value

--- autonomics.support/R/has_identical_values.R
+++ autonomics.support/R/has_identical_values.R
@@ -9,15 +9,3 @@
 has_identical_values <- function(x){
   length(unique(x))==1
 }
-
-#' All have value
-#' @param x vector
-#' @param y scalar
-#' @examples 
-#' require(magrittr)
-#' c(1,1,1,1) %>% all_have_value(1)
-#' @return logical(1)
-#' @export
-all_have_value <- function(x, y){
-  all(x==y)
-}
