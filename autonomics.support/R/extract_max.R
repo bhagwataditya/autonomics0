797889900516f17146ff3c24200332bf72b9b30c
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-07-17 08:51:05 +0200

    rm unused function extract_max

--- autonomics.support/R/extract_max.R
+++ autonomics.support/R/extract_max.R
@@ -33,15 +33,3 @@ is_max <- function(x){
   na_aware_equals(x, max(x, na.rm = TRUE))
 }
 
-#' Extract portion of vector that contains maximum values
-#' @param x numeric vector
-#' @examples
-#' require(magrittr) 
-#' x <- c(A=1,B=3,C=2,D=3, E=NA)
-#' x %>% autonomics.support::extract_max()
-#' @importFrom magrittr %>% 
-#' @export
-extract_max <- function(x){
-  assertive.types::assert_is_numeric(x)
-  x %>% magrittr::extract(autonomics.support::is_max(.))
-}
