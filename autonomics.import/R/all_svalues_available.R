cabd6eb20e281b1c187e323e7463e6efea3c3170
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-07-17 08:42:24 +0200

    mv all_are_missing_or_empty_character to ass_svalues_available.R

--- autonomics.import/R/all_svalues_available.R
+++ autonomics.import/R/all_svalues_available.R
@@ -1,3 +1,29 @@
+
+#' Is missing or empty character
+#' @param x character vector
+#' @return logical vector
+#' @export
+is_missing_or_empty_character <- function(x){
+  x[is.na(x)] <- ''
+  x == ''
+}
+
+
+#' Are all missing or empty character
+#' @param x character vector
+#' @return logical(1)
+#' @examples
+#' require(magrittr)
+#' x <- c('a', 'b', NA_character_, 'd', '')
+#' x %>% autonomics.support::all_are_missing_or_empty_character()
+#' @importFrom magrittr %>%
+#' @export
+all_are_missing_or_empty_character <- function(x){
+  x %>% is_missing_or_empty_character() %>% all()
+}
+
+
+
 #' Are all svalues available?
 #' @param object SummarizedExperiment
 #' @param svar   character(1)
@@ -6,7 +32,7 @@
 #' @export
 all_svalues_available <- function(object, svar){
    svalues1 <- object %>% autonomics.import::svalues(svar)
-   if (is.null(svalues1))                                                return(FALSE)
-   if (autonomics.support::all_are_missing_or_empty_character(svalues1)) return(FALSE)
+   if (is.null(svalues1))                            return(FALSE)
+   if (all_are_missing_or_empty_character(svalues1)) return(FALSE)
    TRUE
 }
