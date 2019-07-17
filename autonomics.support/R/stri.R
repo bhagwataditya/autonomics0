9c5b0c609de9b6b4a372bf141de24e00f411c350
Aditya Bhagwat <aditya.bhagwat@mpi-bn.mpg.de>
2019-07-17 08:17:41 +0200

    rm unused function vstri_detect_fixed

--- autonomics.support/R/stri.R
+++ autonomics.support/R/stri.R
@@ -1,32 +1,3 @@
-#' Vectorized stri_detect_fixed
-#' @param x character vector
-#' @param pattern_vector character vector
-#' @examples
-#' require(magrittr)
-#' x <- c(
-#'    "Intensity L", 
-#'    "Intensity M", 
-#'    "Intensity H",
-#'    "Intensity L E(L).EM(M).BM(H).R1", 
-#'    "Intensity M E(L).EM(M).BM(H).R1", 
-#'    "Intensity H E(L).EM(M).BM(H).R1",
-#'    "Intensity BM(L).E(M).EM(H).R2",   
-#'    "Intensity L BM(L).E(M).EM(H).R2", 
-#'    "Intensity M BM(L).E(M).EM(H).R2",
-#'    "Intensity H BM(L).E(M).EM(H).R2", 
-#'    "Intensity EM(L).BM(M).E(H).R3",   
-#'    "Intensity L EM(L).BM(M).E(H).R3",
-#'    "Intensity M EM(L).BM(M).E(H).R3", 
-#'    "Intensity H EM(L).BM(M).E(H).R3")
-#' pattern_vector <- c("E(L).EM(M).BM(H).R1", "BM(L).E(M).EM(H).R2", "EM(L).BM(M).E(H).R3")
-#' x %>% autonomics.support::vstri_detect_fixed(pattern_vector)
-#' @importFrom magrittr %>%
-#' @export
-vstri_detect_fixed <- function(x, pattern_vector){
-   pattern_vector %>%
-   lapply(function(pattern) x %>% stringi::stri_detect_fixed(pattern)) %>%
-   Reduce('|', .)
-}
 
 #' Vectorized stri_replace_first_fixed
 #' @param x character vector
