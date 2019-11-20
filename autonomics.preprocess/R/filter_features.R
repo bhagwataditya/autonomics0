5245992ba46658dab232ccee9f7e4e57e9c05429
r-cheologist <johannes.graumann@mpi-bn.mpg.de>
2019-11-20 18:54:35 +0000

    Extend feature accounting to filtration functions in `autonaomics.preprocess`;

--- autonomics.preprocess/R/filter_features.R
+++ autonomics.preprocess/R/filter_features.R
@@ -10,7 +10,13 @@ filter_features_nonzero_in_some_sample <- function(object, verbose = TRUE){
    selector <- matrixStats::rowAnys(autonomics.import::exprs(object) != 0, na.rm = TRUE)
    if (verbose) autonomics.support::cmessage('\t\tRetain %d/%d features: non-zero, non-NA, and non-NaN for some sample',
                                               sum(selector), length(selector))
-   object %>% magrittr::extract(selector, )
+   object %<>% magrittr::extract(selector, )
+   if (!is.null(analysis(object))) {
+      analysis(object)$nfeatures %<>%
+         c(structure(
+            sum(selector),
+            names = "non-zero, non-NA, and non-NaN for some sample"))
+   object
 }
 
 #' Keep features that are non zero (and non-NA) for two samples in some subgroup
@@ -23,7 +29,13 @@ filter_features_nonzero_for_two_samples_in_some_subgroup <- function(object){
    n_nonzero_per_subgroup <- stats::aggregate(x, by = list(subgroup = object$subgroup), FUN = function(y){sum(y != 0, na.rm = TRUE)})
    selector <- n_nonzero_per_subgroup %>% magrittr::extract(, -1) %>% magrittr::is_weakly_greater_than(2) %>% matrixStats::colAnys()
    autonomics.support::cmessage('\t\tRetain %d/%d features: non-zero (and non-NA) for at least two samples in some subgroup', sum(selector), length(selector))
-   object[selector, ]
+   object %<>% magrittr::extract(selector, )
+   if (!is.null(analysis(object))) {
+      analysis(object)$nfeatures %<>%
+         c(structure(
+            sum(selector),
+            names = "non-zero and non-NA for at least two samples in some subgroup"))
+      object
 }
 
 
@@ -54,6 +66,12 @@ filter_features_available_in_all_samples <- function(object){
    selector <- object %>% is_available_in_all_samples()
    autonomics.support::cmessage('\t\tUse %d/%d features with available value for each sample', sum(selector), length(selector))
    object %<>% magrittr::extract(selector, )
+   if (!is.null(analysis(object))) {
+      analysis(object)$nfeatures %<>%
+         c(structure(
+            sum(selector),
+            names = "available value for each sample"))
+      object
    object
 }
 
